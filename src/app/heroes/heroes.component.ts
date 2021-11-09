15444ce8a012c9de0f98418f96d3ec1b1740a76f
mattriney <61891660+mattriney@users.noreply.github.com>
2021-11-08 22:41:05 -0600

    update heroes component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -7,20 +7,18 @@ import { HEROES } from '../mock-heroes';
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
 
   heroes = HEROES;
   selectedHero?: Hero;
 
-
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
-
-
 }
